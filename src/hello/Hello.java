31a3aa1e5ac462869d62d62872580616204824ef
王振龙 <1262120514@qq.com>
2018-03-16 17:42:18 +0800

    first

---
+++ src/hello/Hello.java
@@ -0,0 +1,20 @@
+/**  
+* <p>Title: Hello.java</p>  
+* <p>Description: </p>    
+* <p>Company: 自由人</p>  
+* @author 王振龙  
+* @date 2018年3月16日  
+* @version 1.0  
+*/  
+package hello;
+
+
+/**  
+* <p>Title: Hello</p>  
+* <p>Description: </p>  
+* @author 王振龙  
+* @date 2018年3月16日  
+*/
+public class Hello {
+
+}
